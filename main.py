112eeb3ca30932b558b43cc539d1bf043a2341ee
Alexandr Larionov <alarionov93@yandex.ru>
2021-11-17 01:13:57 +0500

    reorganized file checks

--- main.py
+++ main.py
@@ -39,8 +39,10 @@ try:
             start = res.group(2).split('-')[0]
             end = res.group(2).split('-')[1]
             try:
-                if not os.path.isfile('%sIMG_%s_cut_%s_%s.MOV' % (work_dir, file_number, start, end)):
-                    ffmpeg.input(filename).filter('trim', start=start, end=end).output('%sIMG_%s_cut_%s_%s.mov' % (work_dir, file_number, start, end)).run()
+                path_to_chk = '%sIMG_%s_cut_%s_%s.MOV' % (work_dir, file_number, start, end)
+                print(path_to_chk)
+                # if not os.path.isfile(path_to_chk):
+                    # ffmpeg.input(filename).filter('trim', start=start, end=end).output('%sIMG_%s_cut_%s_%s.mov' % (work_dir, file_number, start, end)).run()
             except ffmpeg._run.Error:
                 print('No such file! %s' % 'IMG_%s.MOV' % res.group(1))
         elif res_m:
