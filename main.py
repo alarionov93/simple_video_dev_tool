af6fb52456f7a28aba6b56d478fa7105e298e807
Alexandr Larionov <alarionov93@yandex.ru>
2021-11-16 20:20:27 +0500

    err with dev lst path

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except IndexError:
 vids_one_fragment_re = r'^- \[ \] (\d{4}) (\d{1,2}-\d{2})$'
 vids_multiple_re = r'^- \[ \] (\d{4})(\s\d{1,4}-\d{1,4};)+'
 try:
-    for l in open('dev.lst', 'r').readlines():
+    for l in open('%sdev.lst' % work_dir, 'r').readlines():
         res = re.search(vids_one_fragment_re, l)
         res_m = re.search(vids_multiple_re, l)
         if res:
